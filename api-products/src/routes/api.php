29dbf2283342a8ece89e606403214b320dea0dbb
maurocecamore <mauro@cecamore.it>
2026-06-11 13:06:14 +0200

    feat: aggiunta autenticazione con Laravel Sanctum

--- api-products/src/routes/api.php
+++ api-products/src/routes/api.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('products', ProductController::class);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('products', ProductController::class)->except(['index', 'show']);
+});
+
+Route::apiResource('products', ProductController::class)->only(['index', 'show']);
