0d0e5fe271236f21445ee87a5cd8516ebee40c61
maurocecamore <mauro@cecamore.it>
2026-06-11 16:30:19 +0200

    modificato il DatabaseSeeder per poter lavorare senza factory che dipende da faker in prod

--- api-products/src/database/seeders/DatabaseSeeder.php
+++ api-products/src/database/seeders/DatabaseSeeder.php
@@ -16,11 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+	User::create([
+    		'name' => 'Test User',
+    		'email' => 'test@example.com',
+    		'password' => bcrypt('password'),
+	]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'),
-        ]);
     }
 }
